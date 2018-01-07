b8613868c457dc0c18c7d28a0bfec01184fa200f
Ran Alcobi <ran@ngworkshop.co.il>
2018-01-07 16:46:38 +0200

    First commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('./../config/config')
+
+const path = require('path');
+const express = require("express");
+
+const publicPath = path.join(__dirname , '../public')
+const port = process.env.PORT;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+})
+
+
+console.log(__dirname + '/../public')
+
+console.log(publicPath)
