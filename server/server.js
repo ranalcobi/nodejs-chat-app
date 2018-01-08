c5cd8cd8466b8ff82b2472376f4d96a3242363ff
Ran Alcobi <ran@ngworkshop.co.il>
2018-01-08 10:38:07 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -18,11 +18,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected')
     
+    socket.emit('newMessage', {
+        from:'Admin',
+        text: 'Welcome to the chat app!'
+    })
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'new user joind!',
+        createdAt: new Date().getTime()
+    })
 
     socket.on('createMessage', (message) => {
         console.log('Message created on server: ',message);
-        message.createAt = new Date().getTime();
-        io.emit('newMessage', message)
+         message.createAt = new Date().getTime();
+         io.emit('newMessage', message)
+        // socket.broadcast.emit('newMessage', message)
+
     })
     
     socket.on('disconnect', () =>{
